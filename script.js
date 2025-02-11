f2e6c1a9bad5e8f85a618e2ba6eb47f8febb6aea
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-11 13:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,69 @@
 //complete this code
+
 class Rectangle {
-	constructor(width, height){
-		this._width = width;
-		this._height = height;
-	}
-
-	get width(){
-		return this._width;
-	}
-
-	get height(){
-		return this._height;
-	}
-
-	getArea(){
-		return this._width * this._height;
-	}
-}
-
-class Square extends Animal {
-	constructor(side){
-        super(side, side);
-		this.side = side;
+    constructor(width, height) {
+        if (width <= 0 || height <= 0) {
+            throw new Error("width and height must be positive integers.");
+        }
+        this._width = width;
+        this._height = height;
     }
 
-	getPerimeter(){
-		return 4 * this.side;
-	}
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
 }
+
+class Square extends Rectangle {
+    constructor(side) {
+        if (side <= 0) {
+            throw new Error("Side must be a positive integer.");
+        }
+        super(side, side); 
+    }
+
+    getPerimeter() {
+        return 4 * this._width;
+    }
+}
+
+// class Rectangle {
+// 	constructor(width, height){
+// 		this._width = width;
+// 		this._height = height;
+// 	}
+
+// 	get width(){
+// 		return this._width;
+// 	}
+
+// 	get height(){
+// 		return this._height;
+// 	}
+
+// 	getArea(){
+// 		return this._width * this._height;
+// 	}
+// }
+
+// class Square extends Animal {
+// 	constructor(side){
+//         super(side, side);
+// 		this.side = side;
+//     }
+
+// 	getPerimeter(){
+// 		return 4 * this.side;
+// 	}
+// }
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
